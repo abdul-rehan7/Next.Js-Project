fe279658a368f081752b557ff50cbbbff28fdc9b
rehanmirza76 <abdulrehan0317@gmail.com>
2024-08-12 23:48:42 +0500

    update

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -2,60 +2,47 @@
 
 import Image from "next/image";
 import { Tabs } from "@/components/ui/tabs";
+import { MdDevicesOther } from "react-icons/md";
 
 export function TabsDemo() {
   const tabs = [
     {
-      title: "Product",
-      value: "product",
+      title: "Website Development",
+      value: "Website Development",
       content: (
-        <div className="  lg:w-[85vw] w-[85vw] overflow-hidden relative h-auto rounded-2xl p-10 text-xl md:text-4xl font-bold text-black bg-gradient-to-b from-gray-200 to-gray-300">
-          <p>Product Tab here</p>
+        <div className="  lg:w-[85vw] border-[1px] border-gray-300 w-[85vw] flex flex-col justify-center items-center overflow-hidden relative h-auto rounded-2xl p-10 space-y-2 text-xl md:text-4xl font-bold text-black  bg-gray-100">
+          <MdDevicesOther className="text-9xl" />
+          <p>Website Development</p>
+          <p className="text-center font-normal text-base lg:w-[70%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, beatae earum. Illum saepe cupiditate cumque sit vero minima reiciendis rem nostrum natus voluptatum repellat unde ad mollitia, ipsam numquam ut?</p>
         </div>
       ),
     },
     {
-      title: "Services",
-      value: "services",
+      title: "E-Commerce Web",
+      value: "E-Commerce Web",
       content: (
-        <div className="  lg:w-[85vw] w-[85vw]  overflow-hidden relative h-auto rounded-2xl p-10 text-xl md:text-4xl font-bold text-black bg-gradient-to-b from-gray-200 to-gray-300">
-          <p>Services tab</p>
+        <div className="lg:w-[85vw] border-[1px] border-gray-300 w-[85vw] flex flex-col justify-center items-center   overflow-hidden relative h-auto rounded-2xl space-y-2 p-10 text-xl md:text-4xl font-bold text-black  bg-gray-100">
+          <MdDevicesOther className="text-9xl" />
+          <p>E-Commerce Web</p>
+          <p className="text-center font-normal text-base lg:w-[70%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, beatae earum. Illum saepe cupiditate cumque sit vero minima reiciendis rem nostrum natus voluptatum repellat unde ad mollitia, ipsam numquam ut?</p>
         </div>
       ),
     },
     {
-      title: "Playground",
-      value: "playground",
+      title: "UI/UX Design",
+      value: "UI/UX Design",
       content: (
-        <div className="lg:w-[85vw] w-[85vw]   overflow-hidden relative h-auto rounded-2xl p-10 text-xl md:text-4xl font-bold text-black bg-gradient-to-b from-gray-200 to-gray-300">
-          <p>Playground tab</p>
-        </div>
-      ),
-    },
-    {
-      title: "Content",
-      value: "content",
-      content: (
-        <div className="lg:w-[85vw] w-[85vw]  flex justify-start overflow-hidden  h-auto rounded-2xl p-10 text-xl md:text-4xl font-bold text-black bg-gradient-to-b from-gray-200 to-gray-300">
-          <p>Content tab</p>
-        </div>
-      ),
-    },
-    {
-      title: "Random",
-      value: "random",
-      content: (
-        <div className="lg:w-[85vw] w-[85vw]  overflow-hidden p-10  h-auto rounded-2xl  text-xl md:text-4xl font-bold text-black bg-gradient-to-b from-gray-200 to-gray-300">
-          <p>Random tab</p>
-          <div className="bg-red-500 h-[20rem] w-[20rem]"></div>
-    
+        <div className="  lg:w-[85vw] border-[1px] border-gray-300 w-[85vw] flex flex-col justify-center items-center  overflow-hidden relative h-auto rounded-2xl space-y-2 p-10 text-xl md:text-4xl font-bold text-black bg-gray-100">
+          <MdDevicesOther className="text-9xl" />
+          <p>UI/UX Design </p>
+          <p className="text-center font-normal text-base lg:w-[70%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, beatae earum. Illum saepe cupiditate cumque sit vero minima reiciendis rem nostrum natus voluptatum repellat unde ad mollitia, ipsam numquam ut?</p>
         </div>
       ),
     },
   ];
 
   return (
-    <div className="h-[45rem] md:h-[50rem] [perspective:1000px] relative  flex flex-col max-w-md lg:max-w-6xl  w-full  items-center justify-start lg:py-20 py-10">
+    <div className="h-[45rem] md:h-[50rem]  [perspective:1000px] relative  flex flex-col max-w-md lg:max-w-6xl  w-full  items-center justify-start lg:py-20 py-10">
       <Tabs tabs={tabs} />
     </div>
   );
