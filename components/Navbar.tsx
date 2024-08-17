f7566180fc2bb8aa2bb8acf14271c2b0cbc038e5
rehanmirza76 <abdulrehan0317@gmail.com>
2024-08-17 17:50:52 +0500

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const NewNav = () => {
 
   return (
     <>
-      <div className="bg-blue-500 absolute z-[20] px-6 py-4 flex justify-between items-center w-full">
+      <div className="bg-blue-100 absolute z-[20] px-6 py-3 flex justify-between items-center w-full">
         <div className="logo text-2xl font-bold">Abdul Rehan</div>
         <button
           className="cursor-pointer text-white rounded lg:hidden"
@@ -20,15 +20,23 @@ const NewNav = () => {
           </div>
         </button>
         <div className="lg:flex hidden space-x-5 p-2">
-          <a className="link">Home</a>
-          <a className="link">About</a>
-          <a className="link">Services</a>
-          <a className="link">Contact</a>
+          <a href="#home" className="link">
+            Home
+          </a>
+          <a href="#projects" className="link">
+            Projects
+          </a>
+          <a href="#about" className="link">
+            About
+          </a>
+          <a href="#contact" className="link">
+            Contact
+          </a>
         </div>
       </div>
 
       <div
-        className={`fixed top-0 right-0 bg-green-500 z-[100] p-2 px-4 text-white h-full flex w-[50%] transition-transform duration-500 ${
+        className={`fixed top-0 right-0 shadow-[#0000006f] backdrop-blur-md  shadow-md bg-[#ffffff45] z-[100] p-2 px-4 text-black h-full flex w-[50%] transition-transform duration-500 ${
           visible ? "translate-x-0" : "translate-x-full"
         }`}
       >
@@ -39,16 +47,32 @@ const NewNav = () => {
           />
 
           <div className="flex flex-col space-y-4">
-            <a className="link w-[10rem]" href="">
+            <a
+              className="link w-[10rem]"
+              href="#home"
+              onClick={() => setVisible(false)}
+            >
               Home
             </a>
-            <a className="link w-[10rem]" href="">
+            <a
+              className="link w-[10rem]"
+              href="#projects"
+              onClick={() => setVisible(false)}
+            >
+              Projects
+            </a>
+            <a
+              className="link w-[10rem]"
+              href="#about"
+              onClick={() => setVisible(false)}
+            >
               About
             </a>
-            <a className="link w-[10rem]" href="">
-              Services
-            </a>
-            <a className="link w-[10rem]" href="">
+            <a
+              className="link w-[10rem]"
+              href="#contact"
+              onClick={() => setVisible(false)}
+            >
               Contact
             </a>
           </div>
--- components/ui/Bg.tsx
+++ components/ui/Bg.tsx
@@ -1,7 +1,14 @@
+"use client";
 import React from "react";
 import { TextGenerateEffectDemo } from "@/components/TextGen";
+import { useEffect } from "react";
+import Aos from "aos";
+import 'aos/dist/aos.css'
 
 export function GridBackgroundDemo() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
     <div className="lg:h-[50rem] h-[70vh]  py-5 w-full bg-white bg-grid-black/[0.1] relative flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
@@ -10,7 +17,7 @@ export function GridBackgroundDemo() {
       <div className="z-[10] p-24 justify-center items-center lg:text-left text-center h-screen lg:pt-7 pt-14  w-screen lg:p-12 flex flex-col lg:flex-row ">
         <div className="first p-4 flex flex-col  w-[80vw] lg:w-[55vw] lg:h-[60vh] h-[30vh] ">
           <div >
-            <TextGenerateEffectDemo  />
+            <TextGenerateEffectDemo />
           </div>
         </div>
         <div className="second  mt-7 lg:flex hidden lg:justify-end">
@@ -18,6 +25,7 @@ export function GridBackgroundDemo() {
             src="/hero-item.png"
             className="lg:w-[40rem] lg:h-[30rem] overflow-x-hidden  h-[20rem] w-[20rem]"
             alt=""
+            data-aos = "zoom-in"
           />
         </div>
       </div>
