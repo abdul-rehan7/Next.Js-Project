2579c1983759cec71cfc05f4c26cff2113bc934f
rehanmirza76 <abdulrehan0317@gmail.com>
2024-10-08 11:25:02 +0500

    update

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -11,7 +11,7 @@ export default function Resume() {
     <div className="w-full py-14 px-4 flex flex-col lg:flex-row lg:justify-center lg:space-x-7 space-y-16 ">
       <div
         data-aos="fade-up"
-        className=" lg:w-[40%] lg:h-[32rem] rounded-md  bg-black-300 border broder-black-200/[0.7] hover:bg-gray-300 transition-all text-black-200 p-4"
+        className=" lg:w-[40%] lg:h-[32rem] rounded-md  bg-black-300 border broder-black-200/[0.7] hover:bg-[#333333] transition-all text-black-200 p-4"
       >
         <h1 className="lg:-mt-[11%] -mt-[14%] lg:text-5xl text-3xl text-left font-semibold text-black-200">
           Education
@@ -26,7 +26,7 @@ export default function Resume() {
           </p>
           <p className="lg:text-base text-xs">Next Js Developer</p>
           <p className="lg:text-base text-xs">Web Development Instructor</p>
-          <span className="bg-black-300 w-full h-[1px]"></span>
+          <span className="bg-black-200 w-full h-[1px]"></span>
           <p className="lg:text-base text-xs">
             Army Public College Saddar, Karachi, Pakistan &#40;Aug 2022 to Aug
             2024&#41;
@@ -36,9 +36,9 @@ export default function Resume() {
       </div>
       <div
         data-aos="fade-up"
-        className=" lg:w-[40%] lg:h-[32rem] rounded-md  bg-black-300 border broder-black-200/[0.7]  mt-28 text-black-200 hover:bg-gray-300 transition-all p-4"
+        className=" lg:w-[40%] lg:h-[32rem] rounded-md  bg-black-300 border broder-black-200/[0.7]  mt-28 text-black-200 hover:bg-[#333333] transition-all p-4"
       >
-        <h1 className="lg:-mt-[11%] text-black-200  -mt-[14%] lg:text-5xl text-3xl text-left font-semibold">
+        <h1 className="lg:-mt-[11%] text-black-200 -mt-[14%] lg:text-5xl text-3xl text-left font-semibold">
           Experience
         </h1>
         <div className="items  py-7 flex flex-col space-y-5">
@@ -49,21 +49,21 @@ export default function Resume() {
             Governor Sindh Initiative for GenAI, Web3, and Metaverse Feb 2024 -
             PRESENT
           </p>
-          <span className="bg-black-300 w-full h-[1px]"></span>
+          <span className="bg-black-200 w-full h-[1px]"></span>
           <p className="uppercase lg:text-base text-sm tracking-wider font-medium ">
             HTML/CSS/JS Instructor
           </p>
           <p className="lg:text-base text-xs">
             Erudite Coaching Centre &#40; April 2024 - July 2024 &#41;
           </p>
-          <span className="bg-black-300 w-full h-[1px]"></span>
+          <span className="bg-black-200 w-full h-[1px]"></span>
           <p className="uppercase lg:text-base text-sm tracking-wider font-medium ">
             Website Developer
           </p>
           <p className="lg:text-base text-xs">
             A-FORWARD &#40; Aug 2023 - Nov 2023 &#41;
           </p>
-          <span className="bg-black-300 w-full h-[1px]"></span>
+          <span className="bg-black-200 w-full h-[1px]"></span>
           <p className="uppercase lg:text-base text-sm tracking-wider font-medium ">
             Customer Support Representative
           </p>
