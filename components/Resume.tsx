b6ceceec4ff1cefc4e9467751d26f794d4acfd22
rehanmirza76 <abdulrehan0317@gmail.com>
2025-02-14 22:02:58 +0500

    update

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -11,7 +11,7 @@ export default function Resume() {
     <div className="w-[100vw] py-14 px-4 flex flex-col lg:flex-row lg:justify-center lg:space-x-7 space-y-16 ">
       <div
         data-aos="fade-up"
-        className=" lg:w-[40%] lg:h-[32rem] rounded-md  bg-black-300 border broder-black-200/[0.7] hover:bg-gray-200 transition-all text-black-200 p-4"
+        className=" lg:w-[40%] lg:h-[32rem] rounded-md  bg-black-300 border broder-black/[0.9] hover:bg-gray-200 transition-all text-black-200 p-4"
       >
         <h1 className="lg:-mt-[15%] -mt-[14%] lg:text-5xl text-3xl text-left font-semibold text-black-200">
           Education
